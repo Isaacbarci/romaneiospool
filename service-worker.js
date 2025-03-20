cb8723e47c68ec0248ce276726f0eb663004afa8
Isaac <115880314+Isaacbarci@users.noreply.github.com>
2025-03-20 07:36:56 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,52 @@
 const CACHE_VERSION = new Date().getTime();
 const CACHE_NAME = `romaneio-cache-${CACHE_VERSION}`;
 
-self.addEventListener("install", (event) => {
+// Função para adicionar um arquivo ao cache
+const addToCache = async (cacheName, file) => {
+    const cache = await caches.open(cacheName);
+    await cache.add(file);
+};
+
+// Instalação do Service Worker
+self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            console.log("Cache atualizado:", CACHE_NAME);
-            return cache.addAll([
-                "/",
-                "index.html",
-                "style.css",
-                "script.js"
-            ]);
-        })
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll([
+                '/',
+                'index.html',
+                'image.png',
+                'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.17.4/xlsx.full.min.js',
+                'https://unpkg.com/html5-qrcode',
+            ]))
     );
-    self.skipWaiting(); // Ativa o novo Service Worker imediatamente
 });
 
-self.addEventListener("activate", (event) => {
-    // Não removemos caches antigos.
-    self.clients.claim(); // Garante que o novo Service Worker assuma o controle imediatamente
-});
-
-self.addEventListener("fetch", (event) => {
+// Intercepta as requisições e serve do cache
+self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
-            // Se o recurso estiver no cache, ele é retornado
-            if (response) {
-                return response;
-            }
-            // Caso contrário, realiza a requisição na rede
-            return fetch(event.request);
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});
+
+// Mensagem para adicionar um novo arquivo ao cache
+self.addEventListener('message', (event) => {
+    if (event.data.action === 'addToCache') {
+        addToCache(CACHE_NAME, event.data.file);
+    }
+});
+
+// Atualiza o cache quando houver uma nova versão
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache); // Remove caches antigos
+                    }
+                })
+            );
         })
     );
 });
